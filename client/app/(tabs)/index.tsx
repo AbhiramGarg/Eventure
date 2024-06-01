386b2ba4ccf9e1fff54c417a744030089741f95a
AbhiramGarg <abhiramgargeia@gmail.com>
2024-06-01 10:55:33 -0700

    getting to know how native works

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/gojowp.png')}
           style={styles.reactLogo}
         />
       }>
@@ -20,30 +20,30 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+        <ThemedText style={styles.subtitleContainer} type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText style={styles.paratext}>
+          Edit <ThemedText style={styles.paratext} type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
-          <ThemedText type="defaultSemiBold">
+          <ThemedText style={styles.paratext} type="defaultSemiBold">
             {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
           </ThemedText>{' '}
           to open developer tools.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
+        <ThemedText style={styles.subtitleContainer} type="subtitle">Step 2: Explore</ThemedText>
+        <ThemedText style={styles.paratext}>
           Tap the Explore tab to learn more about what's included in this starter app.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
+        <ThemedText style={styles.subtitleContainer} type="subtitle">Step 3: Get a fresh start</ThemedText>
+        <ThemedText style={styles.paratext} >
           When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+          <ThemedText style={styles.paratext} type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+          <ThemedText style={styles.paratext} type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+          <ThemedText style={styles.paratext} type="defaultSemiBold">app</ThemedText> to{' '}
+          <ThemedText style={styles.paratext} type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
@@ -61,10 +61,16 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: 258,
+    width: 390,
     bottom: 0,
     left: 0,
     position: 'absolute',
   },
+  subtitleContainer:{
+    color:'#1eb4eb'
+  },
+  paratext:{
+    color:'#96c5d6'
+  }
 });
